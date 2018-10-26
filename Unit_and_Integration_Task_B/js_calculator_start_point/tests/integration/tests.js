43f0695d8b2f471d36eb2b7d99ae6ade90159b65
ricardoruizfp <rfpimpao@gmail.com>
2018-10-26 16:36:45 +0100

    Task B complete

--- Unit_and_Integration_Task_B/js_calculator_start_point/tests/integration/tests.js
+++ Unit_and_Integration_Task_B/js_calculator_start_point/tests/integration/tests.js
@@ -12,8 +12,79 @@ describe('calculator functionality', function() {
   // write integration tests here in the form of "it should do something..."
   it('should have working number buttons', function(){
     running_total = element(by.css('#running_total'))
+    element(by.css('#number1')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('1')
+  })
+
+  it('should be able to update the display of the running total', function() {
+    running_total = element(by.css('#running_total'));
+    element(by.css('#number1')).click();
+    element(by.css('#number1')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('11');
+  })
+
+  it('should be able to update the display after an operation', function() {
+    running_total = element(by.css('#running_total'));
+    element(by.css('#number1')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number1')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('2');
+  })
+
+  it('should be able to update the after chaining operations', function() {
+    running_total = element(by.css('#running_total'));
+    element(by.css('#number1')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number1')).click();
+    element(by.css('#operator_multiply')).click();
     element(by.css('#number2')).click();
-    expect(running_total.getAttribute('value')).to.eventually.equal('2')
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('4');
+  })
+
+  it('should be able display negative numbers', function() {
+    running_total = element(by.css('#running_total'));
+    element(by.css('#number1')).click();
+    element(by.css('#operator_subtract')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('-1');
+  })
+
+  it('should be able display decimal numbers', function() {
+    running_total = element(by.css('#running_total'));
+    element(by.css('#number3')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('1.5');
+  })
+
+  it('should be able display large numbers', function() {
+    running_total = element(by.css('#running_total'));
+    element(by.css('#number9')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('997002999');
+  })
+
+  it('should display not a number when dividing by zero', function() {
+    running_total = element(by.css('#running_total'));
+    element(by.css('#number5')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('Not a number');
   })
 
 });
--- Unit_and_Integration_Task_B/js_calculator_start_point/tests/unit/calculator_spec.js
+++ Unit_and_Integration_Task_B/js_calculator_start_point/tests/unit/calculator_spec.js
@@ -6,9 +6,58 @@ describe('calculator', function () {
     calculator = new Calculator()
   });
 
-  // write unit tests here in the form of "it should do something..."
-  it('it has a sample test', function(){
-    assert.equal(true, true)
+  // unit
+
+  it('can add two numbers', function() {
+    calculator.previousTotal = 1;
+    calculator.add(2);
+    assert.equal(3, calculator.runningTotal);
   })
 
+  it('can subtract two numbers', function() {
+    calculator.previousTotal = 2;
+    calculator.subtract(1);
+    assert.equal(1, calculator.runningTotal);
+  })
+
+  it('can multiply two numbers', function() {
+    calculator.previousTotal = 2;
+    calculator.multiply(2);
+    assert.equal(4, calculator.runningTotal);
+  })
+
+  it('can divide two numbers', function() {
+    calculator.previousTotal = 4;
+    calculator.divide(2);
+    assert.equal(2, calculator.runningTotal);
+  })
+
+  // integration
+  
+  it('should be able to concatenate multiple number button clicks', function() {
+    calculator.numberClick(1);
+    calculator.numberClick(2);
+    calculator.numberClick(3);
+    assert.equal(123, calculator.runningTotal);
+  })
+
+  it('should be able to chain multiple operations together', function() {
+    calculator.runningTotal = 1;
+    calculator.operatorClick('+');
+    calculator.numberClick(1);
+    calculator.operatorClick('-');
+    calculator.numberClick(1)
+    calculator.operatorClick('=')
+    assert.equal(1, calculator.runningTotal);
+  })
+
+  it('should be able to clear the running total without affecting the calculation', function() {
+    calculator.runningTotal = 1;
+    calculator.operatorClick('+');
+    calculator.numberClick(1);
+    calculator.clearClick();
+    calculator.numberClick(1);
+    calculator.operatorClick('=');
+    assert.equal(2, calculator.runningTotal);
+  })
 });
